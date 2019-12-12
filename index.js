20e24db1734698b9e17611f9d14173c143e62eb3
flowersfaerie <flowersfaerie@gmail.com>
2019-12-12 14:56:55 -0800

    Completed: Student class challenge.

--- index.js
+++ index.js
@@ -178,8 +178,25 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
-
+class Student extends Lambdasian {
+	constructor(studentAttr) {
+		super(studentAttr);
+		this.previousBackground = studentAttr.previousBackground;
+		this.className = studentAttr.className;
+		this.favSubjects = studentAttr.favSubjects;
+	}
+	
+	listSubjects() {
+		return this.favSubjects.toString();
+	}
+	
+	PRAssignment(subject) {
+		return `${this.name} has submitted a PR for ${subject}`;
+	}
+	
+	sprintChallenge(subject) {
+		return `${this.name} has begun sprint challenge on ${subject}`;
+	}
 }
 
 /*
